42e91d64f3c9ea407db4e406107769e30a81d21e
sortKing <13058551224@163.com>
2023-09-07 22:27:54 +0800

    初始化

--- thread_pool.hpp
+++ thread_pool.hpp
@@ -1,5 +1,4 @@
 #pragma once
-<<<<<<< HEAD
 #include <functional>
 #include <future>
 #include <mutex>
@@ -148,97 +147,5 @@ void worker::work()
     } while (true);
     status = off;
 }
-=======
-
-#include "cuque.hpp"
-#include "type_traits.hpp"
-#include <functional>
-#include <future>
-#include <mutex>
-#include <list>
-#include <thread>
-#include <vector>
-
-namespace my {
-
-    class thread_pool {
-        std::mutex mtx;
-        std::condition_variable cnd;
-        my::cuque<std::function<void()>> task;
-        std::atomic_size_t count{0};
-        std::atomic_int th_num{0};
-        std::list < std::thread > pool;
-
-    public:
-        size_t size() {
-            return th_num;
-        }
-
-    private:
-
-        void work() {
-            do {
-                std::function<void()> fun;
-                {
-                    std::unique_lock<std::mutex> lock(mtx);
-                    while (!task.pull_front(fun)) {
-                        cnd.wait(lock);
-                    }
-                }
-                if (fun) {
-                    fun();
-                    count -= 1;
-                } else {
-                    break;
-                }
-            } while (true);
-        }
-
-    public:
-        template<class F, class... Args>
-        std::future<my::invoke_result_t<F, Args...>> add(F &&fun, Args &&...args) {
-            using type = std::packaged_task<my::invoke_result_t<F, Args...>()>;
-            auto task_ptr = std::make_shared<type>(std::bind(std::forward<F>(fun), std::forward<Args>(args)...));
-            count += 1;
-            task.emplace_back([=] { (*task_ptr)(); });
-            cnd.notify_one();
-            return task_ptr->get_future();
-        }
-
-        void wait() {
-            std::function<void()> fun;
-            while (count != 0) {
-                if (task.pull_front(fun)) {
-                    fun();
-                    count -= 1;
-                } else {
-                    std::this_thread::yield();
-                }
-            }
-        }
-
-        explicit thread_pool(size_t n) {
-            for (size_t i = n; i != 0; --i) {
-                pool.emplace_back([&] {
-                    th_num += 1;
-                    work();
-                    th_num -= 1;
-                });
-            }
-        }
-
-        ~thread_pool() {
-            for (int i = th_num; i != 0; --i) {
-                task.emplace_back();
-                cnd.notify_one();
-            }
-            println("µÈ´ý");
-            for (auto & th : pool) {
-                th.join();
-            }
-            println("Íê³É");
-        }
-    };
->>>>>>> 6eba88305f037d296a18d55a228d6267110dec94
 
 } // namespace my
--- type_traits.hpp
+++ type_traits.hpp
@@ -4,7 +4,6 @@
 
 namespace my {
 
-<<<<<<< HEAD
 template <class F, class... ArgTypes>
 class invoke_result {
 public:
@@ -13,14 +12,5 @@ public:
 
 template <class F, class... ArgTypes>
 using invoke_result_t = typename invoke_result<F, ArgTypes...>::type;
-=======
-    template<class F, class... ArgTypes>
-    class invoke_result {
-    public:
-        using type = decltype(std::declval<F>()(std::declval<ArgTypes>()...));
-    };
-
-    template<class F, class... ArgTypes> using invoke_result_t = typename invoke_result<F, ArgTypes...>::type;
->>>>>>> 6eba88305f037d296a18d55a228d6267110dec94
 
 } // namespace my
